ebb26fe94dd5f256c192473300a966286009d3ed
HadertS <source.zero@googlemail.com>
2026-03-05 16:53:42 +0000

    changed default scene and added look_at functionality to movement component

--- Script/Component/Input/input_component.gd
+++ Script/Component/Input/input_component.gd
@@ -6,4 +6,3 @@ var move_direction: Vector2 = Vector2.ZERO
 func update():
     # Read input and assemble movement vector based on default Godot input actions.
     move_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-    print("move_direction: ", move_direction)
--- Script/Component/Movement/movement_component.gd
+++ Script/Component/Movement/movement_component.gd
@@ -1,17 +1,18 @@
 class_name MovementComponent extends Node
 
 @export var body: CharacterBody2D
-@export var sprite: Node2D
+@export var model: Node2D
 @export var speed := 100.0
 
 var direction: Vector2 = Vector2.ZERO
 
-func tick(delta: float)->void:
+func tick(delta: float) -> void:
     if body == null:
         return
 
     body.velocity.x = direction.x * speed
     body.velocity.y = direction.y * speed
     body.move_and_slide()
-    print("body.velocity: ", body.velocity)
-
+    if model and direction.length() > 0.001:
+        var look_direction := direction.normalized()
+        model.look_at(model.global_position + look_direction)
