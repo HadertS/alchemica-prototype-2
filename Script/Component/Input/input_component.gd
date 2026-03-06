5def3ce17b4792d486ac60d2b41e426279eb524b
HadertS <source.zero@googlemail.com>
2026-03-06 19:46:16 +0000

    added mouse functionality to input_component

--- Script/Component/Input/input_component.gd
+++ Script/Component/Input/input_component.gd
@@ -1,10 +1,15 @@
 class_name InputComponent extends Node
 
-## The normalized direction vector to use for movement.
 var move_direction: Vector2 = Vector2.ZERO
 var interact_pressed: bool = false
+var mouse_position: Vector2 = Vector2.ZERO
+var mouse_clicked: bool = false
 
 func update():
     # Read input and assemble movement vector based on default Godot input actions.
     move_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+    
     interact_pressed = Input.is_action_just_pressed("ui_select")
+    
+    mouse_position = get_viewport().get_mouse_position()
+    mouse_clicked = Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT)
