3f621911ba95759324057a3bc0511a25e8ef3de9
HadertS <source.zero@googlemail.com>
2026-03-07 15:37:41 +0000

    added inventory keyto InputComponent

--- Script/Component/Input/input_component.gd
+++ Script/Component/Input/input_component.gd
@@ -1,7 +1,11 @@
 class_name InputComponent extends Node
 
 var move_direction: Vector2 = Vector2.ZERO
+
 var interact_pressed: bool = false
+var inventory_pressed: bool = false
+
+
 var mouse_position: Vector2 = Vector2.ZERO
 var mouse_clicked: bool = false
 
@@ -10,6 +14,10 @@ func update():
     move_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
     
     interact_pressed = Input.is_action_just_pressed("ui_select")
+    inventory_pressed = Input.is_action_just_pressed("ui_inventory")
     
     mouse_position = get_viewport().get_mouse_position()
     mouse_clicked = Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT)
+
+
+
