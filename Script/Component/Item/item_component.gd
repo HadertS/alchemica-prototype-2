b71983d3e282fac17c1df9519e9ab565e35992d7
HadertS <source.zero@googlemail.com>
2026-03-07 15:57:07 +0000

    removed redundant variables

--- Script/Component/Item/item_component.gd
+++ Script/Component/Item/item_component.gd
@@ -2,11 +2,8 @@ class_name ItemComponent extends Node
 
 @export var item_name: String = ""
 @export var item_id: int = 0
-@export var stackable: bool = false
 @export var max_stack: int = 1
 @export var ammount: int = 1
-
-var amount: int = 1
 
 func use() -> void:
     pass
